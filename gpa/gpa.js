493f03c86404c0b1e77ef26fc81fc03134eaa225
Azizou Ogbone <ogboneazizou@gmail.com>
2016-12-22 17:00:07 +0200

    fix js error on div vaiable

--- gpa/gpa.js
+++ gpa/gpa.js
@@ -6,10 +6,12 @@ function ComputeEngGPA(argument) {
 	year3 = parseInt(form.elements[2].value);
 	year4 = parseInt(form.elements[3].value);
 	var sum = (year4*4) + (year3*3) + (year2*2) + year1;
-	avg = sum/10.0;
+	var avg = sum/10.0;
 	// alert("Eng GPA is " + avg);
-
+	document.getElementById('gpa-form').style.display = "none"; 
 	var result = document.getElementById('result');
-	var res = "Your GPA according to the Engineering department's formula is: " + avg;
-	div.innerHTML += res;
+	result.className += " jumbotron"
+	try_again = "<a class=\"btn btn-primary\" href=\"javascript:history.go(0)\">Compute another GPA?</a>"
+	var res = "<p class='lead'>Your GPA according to the Engineering department's formula is: " + avg + "</p>";
+	result.innerHTML = res + "<br />" + try_again;
 }
